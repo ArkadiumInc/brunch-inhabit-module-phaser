75caaf88a91098bc38cf322b58b1f33f1ad09c24
WishMaster <wmts@arkadium.com>
2016-12-19 23:31:01 -0500

    update brunch configuration

--- generators/app/templates/project/brunch-config.js
+++ generators/app/templates/project/brunch-config.js
@@ -1,22 +1,22 @@
 module.exports = {
-  modules: {
-    autoRequire: {
-      'my-module.js': [ 'my-module.js' ]
+    modules: {
+        autoRequire: {
+            '<%= name %>.js': ['<%= name %>.js']
+        }
+    },
+    files: {
+        javascripts: {
+            joinTo: '<%= name %>.js'
+        }
+    },
+    plugins: {
+        babel: {presets: ['es2015']},
+        text: {pattern: /\.hbs$/}
+    },
+    npm: {
+        globals: {
+            PIXI: 'phaser/build/custom/pixi',
+            p2: 'phaser/build/custom/p2'
+        }
     }
-  },
-  files: {
-    javascripts: {
-      joinTo: 'my-module.js'
-    }
-  },
-  plugins: {
-    babel: {presets: ['es2015']},
-    text: { pattern: /\.hbs$/ }
-  },
-  npm: {
-    globals: {
-      PIXI: 'phaser/build/custom/pixi',
-      p2:   'phaser/build/custom/p2'
-    }
-  }
 };
